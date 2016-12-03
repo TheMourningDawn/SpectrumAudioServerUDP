df09b478f027727eedd5b0c57a73405ccc76a5f1
Charlie Hendricks <themourningdawn@gmail.com>
2016-12-03 14:14:43 -0600

    Added basic functionality of sending one (left) band of frequencies as an array of 7 integers over UDP in a 14 byte packet in big endian order. Cleaned some dead code out for now.

--- SpectrumAudioServerUDP.ino
+++ SpectrumAudioServerUDP.ino
@@ -1,14 +1,15 @@
 #include "application.h"
 #include "SpectrumEqualizer.h"
 
+#define NUM_FREQUENCIES 7
+#define BUFFER_SIZE NUM_FREQUENCIES*2
+
 SpectrumEqualizer *spectrum;
+char spectrumAsBytes[BUFFER_SIZE];
 
 UDP udpMulticast;
-const size_t bufferSize = 16;
-IPAddress remoteIP(239,1,1,234);
-unsigned char buffer[bufferSize];
 int port = 32555;
-int hueValue = 100;
+IPAddress remoteIP(239,1,1,234);
 
 void setup() {
     Serial.begin(9600);
@@ -16,48 +17,21 @@ void setup() {
     udpMulticast.begin(0);
 
     spectrum = new SpectrumEqualizer();
+    spectrum->init();
 }
 
 void loop() {
    spectrum->readAudioFrequencies();
-   char bufferJunk[] = {1, 23, 345, 4095};
-   if(udpMulticast.sendPacket(bufferJunk, bufferSize, remoteIP, port) > 0) {
-       Serial.printlnf("%d", value);
+   for(int i=0;i<BUFFER_SIZE;i+=2) {
+        spectrumAsBytes[i] = spectrum->frequenciesLeft[i/2] >> 8;
+        spectrumAsBytes[i+1] = spectrum->frequenciesLeft[i/2] & 0xFF;
+   }
+
+   if(udpMulticast.sendPacket(spectrumAsBytes, BUFFER_SIZE, remoteIP, port) > 0) {
+       Serial.print(".");
    } else {
        Serial.printlnf("send failed");
        delay(1000);
        udpMulticast.begin(0);
    }
-}
-
-void setupModeFunctions() {
-  /*Particle.function("nextMode", nextMode);
-  Particle.function("previousMode", previousMode);
-
-  Particle.subscribe("NEXT_MODE", handleNextMode);
-  Particle.subscribe("PREVIOUS_MODE", handlePreviousMode);*/
-}
-
-int nextMode(String mode) {
-    /*int currentPattern = animations->nextPattern();
-    char currentPatternString[5];
-    sprintf(currentPatternString, "%i", currentPattern);
-    Particle.publish("Current Pattern", currentPatternString);*/
-    return 1;
-}
-
-void handleNextMode(const char *eventName, const char *data) {
-    nextMode("notSureWhyIHaveToDoItLikeThis");
-}
-
-int previousMode(String mode) {
-    /*int currentPattern = animations->previousPattern();
-    char currentPatternString[5];
-    sprintf(currentPatternString, "%i", currentPattern);
-    Particle.publish("Current Pattern", currentPatternString);*/
-    return 1;
-}
-
-void handlePreviousMode(const char *eventName, const char *data) {
-    previousMode("seriouslyWhy?");
 }
--- SpectrumEqualizer.cpp
+++ SpectrumEqualizer.cpp
@@ -3,29 +3,27 @@
 
 #include "SpectrumEqualizer.h"
 #include "application.h"
-#include "LEDAnimations.h"
 
 SpectrumEqualizer::SpectrumEqualizer() {
 
 }
 
 void SpectrumEqualizer::init() {
-    pinMode(RESET_PIN, OUTPUT); // reset
-    pinMode(STROBE_PIN, OUTPUT); // strobe
-    digitalWrite(RESET_PIN, LOW); // reset low
-    digitalWrite(STROBE_PIN, HIGH); //pin 5 is RESET on the shield
+    pinMode(RESET_PIN, OUTPUT);
+    pinMode(STROBE_PIN, OUTPUT);
+    digitalWrite(RESET_PIN, LOW);
+    digitalWrite(STROBE_PIN, HIGH);
 }
 
 void SpectrumEqualizer::readAudioFrequencies() {
-    // reset the data
     digitalWrite(RESET_PIN, HIGH);
     digitalWrite(RESET_PIN, LOW);
 
     for (int band = 0; band < 7; band++) {
         digitalWrite(STROBE_PIN, LOW); // go to the next band
         delayMicroseconds(50); // gather some data
-        frequenciesLeft[band] = analogRead(LEFT_EQ_PIN); // store left band reading
-        frequenciesRight[band] = analogRead(RIGHT_EQ_PIN); // store right band reading
+        frequenciesLeft[band] = analogRead(LEFT_EQ_PIN);
+        frequenciesRight[band] = analogRead(RIGHT_EQ_PIN);
         digitalWrite(STROBE_PIN, HIGH); // reset the strobe pin
     }
 }
