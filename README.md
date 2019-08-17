b658449b6d4b83d00e071d12dde1d702fa29e50e
charlie.hendricks <charlie.hendricks@equipmentshare.com>
2019-08-17 11:16:13 -0500

    Added cloud functions to allow turning on/off, resetting, and forcing safe mode. Updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SpectrumAudioServerUDP
-Sends audio spectrum data from a Sparkfun Spectrum shield over multicast UDP
+
+Sends audio spectrum data from a Sparkfun Spectrum shield over multicast UDP.
+
+Can be used along with the client: https://github.com/TheMourningDawn/SpectrumEqualizerClient
--- SpectrumAudioServerUDP.ino
+++ SpectrumAudioServerUDP.ino
@@ -9,12 +9,18 @@ char spectrumAsBytes[BUFFER_SIZE];
 
 UDP udpMulticast;
 int port = 32555;
-IPAddress remoteIP(239,1,1,234);
+IPAddress remoteIP(239, 1, 1, 234);
 
 SYSTEM_THREAD(ENABLED);
 
+bool poweredOn = true;
+
 void setup() {
-    /*Serial.begin(11520);*/
+    Particle.function("reset-device", resetDevice);
+    Particle.function("enter-safe-mode", enterSafeMode);
+    Particle.function("power", power);
+
+    Particle.variable("powered-on", poweredOn);
 
     udpMulticast.begin(0);
 
@@ -23,14 +29,39 @@ void setup() {
 }
 
 void loop() {
-   delay(20);
-   spectrum->readAudioFrequencies();
-   for(int i=0;i<BUFFER_SIZE;i+=2) {
-        spectrumAsBytes[i] = spectrum->frequenciesLeft[i/2] >> 8;
-        spectrumAsBytes[i+1] = spectrum->frequenciesLeft[i/2] & 0xFF;
-   }
-   if(udpMulticast.sendPacket(spectrumAsBytes, BUFFER_SIZE, remoteIP, port) < 0) {
-     udpMulticast.begin(0);
-   }
-   /*Serial.printlnf("Sent: [%d, %d, %d, %d]", spectrum->frequenciesLeft[0],spectrum->frequenciesLeft[2],spectrum->frequenciesLeft[4],spectrum->frequenciesLeft[6]);*/
+    delay(20);
+    spectrum->readAudioFrequencies();
+    for (int i = 0; i < BUFFER_SIZE; i += 2) {
+        spectrumAsBytes[i] = spectrum->frequenciesLeft[i / 2] >> 8;
+        spectrumAsBytes[i + 1] = spectrum->frequenciesLeft[i / 2] & 0xFF;
+    }
+    if (udpMulticast.sendPacket(spectrumAsBytes, BUFFER_SIZE, remoteIP, port) < 0) {
+        Particle.publish("Resetting UDP connection.");
+        udpMulticast.begin(0);
+    }
+    /*Serial.printlnf("Sent: [%d, %d, %d, %d]", spectrum->frequenciesLeft[0],spectrum->frequenciesLeft[2],spectrum->frequenciesLeft[4],spectrum->frequenciesLeft[6]);*/
+}
+
+int resetDevice(String arg) {
+    System.reset();
+
+    return 1;
+}
+
+int enterSafeMode(String arg) {
+    System.enterSafeMode();
+
+    return 1;
+}
+
+int power(String arg) {
+    if (arg == "ON") {
+        poweredOn = true;
+    } else if (arg == "OFF") {
+        poweredOn = false;
+    } else {
+        return -1;
+    }
+
+    return 1;
 }
